2651d6e896539638925afc6eb760b7f2ced08708
Jessica Jiang <jessicajiang415@gmail.com>
2024-03-05 19:12:44 -0500

    updated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const myInput = document.getElementById("myInput");
 const listContainer = document.getElementById("list-container");
-
+const dueDate = document.getElementById("due-date");
 
 function addTask() {
     if(myInput.value === ''){
@@ -11,6 +11,11 @@ function addTask() {
         let li = document.createElement("li");
         li.innerHTML = myInput.value;
         listContainer.appendChild(li);
+
+        let date = document.createElement();
+        date.innerHTML = 
+
+
         // add the cross icon to each of the listed tasks
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
@@ -19,25 +24,41 @@ function addTask() {
     myInput.value = "";
     saveData();
 }
-// try to make it so that you can just press enter to add a new task instead of pressing the button every time
-// myInput.addEventListener('keypress', (event) => {
-//     if(event.key === 13) {
-//         addTask();
-//     }
-// });
+
 
 listContainer.addEventListener("click", function(e){
     // when clicking on a specific task it checks and unchecks the task, strike out and unstrikes the task
     if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-        saveData();
+        while (e.target.classList.toggle("checked")){
+            saveData();
+            play();
+        };
     }
+
     // if the cross/x is clicked on then it removes the task listed
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
         saveData();
     }
 }, false);
+
+// document.addEventListener('click', function (event) {
+// 	fadeOutAndRemove(event.target);
+// });
+
+// function fadeOutAndRemove(element) {
+// 	element.classList.add('fade-out');
+//     element.addEventListener('transitionend', function () {
+//   	element.parentNode.removeChild(element);
+//   });
+// }
+
+
+// listContainer.addEventListener("click", ){
+//     if (e.target.tagName === "LI"){
+//         while (e.target.classList)
+//     }
+// }
 
 function saveData(){
     localStorage.setItem("data", listContainer.innerHTML);
@@ -46,5 +67,45 @@ function saveData(){
 function displayTask(){
     listContainer.innerHTML = localStorage.getItem("data");
 }
+
+function play(e) {
+    var audio = document.getElementById("bell");
+    audio.currentTime = 0;
+    audio.play();
+
+// double check the code below with toggleBtnAnimation, make sure it makes sense with my code
+    const btns = document.querySelectorAll('li');
+    btns.forEach(btn => btn.addEventListener('click', toggleBtnAnimation));
+
+}
+
+// // Create a new list item when clicking on the "Add" button
+// function newElement() {
+//     var li = document.createElement("li");
+//     var inputValue = document.getElementById("myInput").value;
+//     var t = document.createTextNode(inputValue);
+//     li.appendChild(t);
+//     if (inputValue === '') {
+//       alert("You must write something!");
+//     } else {
+//       document.getElementById("list-container").appendChild(li);
+//     }
+//     document.getElementById("myInput").value = "";
+  
+//     var span = document.createElement("SPAN");
+//     var txt = document.createTextNode("\u00D7");
+//     span.className = "close";
+//     span.appendChild(txt);
+//     li.appendChild(span);
+  
+//     for (i = 0; i < close.length; i++) {
+//       close[i].onclick = function() {
+//         var div = this.parentElement;
+//         div.style.display = "none";
+//       }
+//     }
+//     saveData();
+//   }
+
 
 displayTask();
