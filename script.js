85dc6623fb86413fc35ca664a386abd2a182788a
Jessica Jiang <jessicajiang415@gmail.com>
2024-03-09 22:37:36 -0500

    final commit

--- script.js
+++ script.js
@@ -2,55 +2,17 @@ const input = document.getElementById('myInput');
 const taskList = document.getElementById('todo-list');
 const completedList = document.getElementById('completed-list');
 const dueDate = document.getElementById("due-date");
-// const todosHtml = document.querySelector(".todos");
-// let todosJson = JSON.parse(localStorage.getItem("todos"))
-// const filters = document.querySelectorAll(".filter");
 
-// let filter = '';
-
-// function addTask() {
-//     if(myInput.value === ''){} 
-//     else {
-//         // upon clicking the add button after you write out your tag, it is listed under the input/add button  
-//         let li = document.createElement("li");
-//         li.innerHTML = `${myInput.value} <p class="due-date">Due Date: ${dueDate.value}</p>`;
-        
-//         // do {
-//         //     li.innerHTML = `<p class="due-date">Due Date: ${dueDate.value}</p>`;
-//         // } while (dueDate.value != '');
-//         listContainer.appendChild(li);
-
-//         // let task = {
-//         //     description: inputBox.value,
-//         //     dueDate: dueDate.value,
-//         //   };
-//         //   compltedTask.push(task);
-//         // //   let li = document.createElement("li");
-//         //   li.innerHTML = `${inputBox.value} <p class="due-date">Due: ${dueDate.value}</p>`;
-//         // //   listContainer.appendChild(li);
-
-//         // add the cross icon to each of the listed tasks
-//         let span = document.createElement("span");
-//         span.innerHTML = "\u00d7";
-//         li.appendChild(span);
-//     }
-//     myInput.value = "";
-//     dueDate.value = "";
-//     saveData();
-// }
 
 document.addEventListener('DOMContentLoaded', function () {
-    // loadTasks();
+    loadTasks();
     loadCompletedTasks();
   });
 
 function addTask() {
     const taskText = input.value.trim();
 
-    // if (taskText !== '') {
-
-    if(input.value === ''){} 
-    else {
+    if (taskText !== '') {
       const newTask = document.createElement('li');
       newTask.innerHTML = `
         ${taskText} <p class="due-date">Due: ${dueDate.value}</p>
@@ -61,10 +23,7 @@ function addTask() {
           <option value="high">High</option>
         </select>`;
       taskList.appendChild(newTask);
-    //   setPriority(newTask.querySelector('select')); // Set initial priority
 
-
-      // add the cross icon to each of the listed tasks
       let span = document.createElement("span");
       span.innerHTML = "\u00d7";
       newTask.appendChild(span);
@@ -78,7 +37,6 @@ function addTask() {
 
 
 taskList.addEventListener("click", function(e){
-    // when clicking on a specific task it checks and unchecks the task, strike out and unstrikes the task
     if(e.target.tagName === "LI"){
         while (e.target.classList.toggle("checked")){
             moveTaskToCompleted(e.target);
@@ -88,7 +46,6 @@ taskList.addEventListener("click", function(e){
         saveTasks();
     }
 
-    // if the cross/x is clicked on then it removes the task listed
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
         saveTasks();
@@ -108,11 +65,6 @@ function saveTasks() {
     localStorage.setItem('tasks', taskList.innerHTML);
     localStorage.setItem('completedTasks', completedList.innerHTML);
 }
-
-// // allows the information/data to be saved, even when you exit/reopen, refresh, and backout of the page 
-// function saveData(){
-//     localStorage.setItem("data", taskList.innerHTML);
-// }
 
 function toggleCompletion(checkbox) {
     const taskText = checkbox.nextElementSibling;
@@ -149,12 +101,6 @@ function toggleCompletion(checkbox) {
     saveTasks();
   }
 
-  function checkEnterKey(event) {
-    if (event.key === 'Enter') {
-      addTask();
-    }
-  }
-
   function showTab(tabName) {
     const todoTab = document.getElementById('todo-tasks');
     const completedTab = document.getElementById('completed-tasks');
@@ -173,31 +119,15 @@ function displayTask(){
     taskList.innerHTML = localStorage.getItem("tasks");
 }
 
-function play(e) {
+function play() {
     var audio = document.getElementById("audio");
     audio.currentTime = 0;
     audio.play();
 
-// double check the code below with toggleBtnAnimation, make sure it makes sense with my code
     const btns = document.querySelectorAll('li');
     btns.forEach(btn => btn.addEventListener('click', toggleBtnAnimation));
     saveTasks();
 
 }
-
-// filters.forEach(function (el) {
-//     el.addEventListener("click", (e) => {
-//       if (el.classList.contains('active')) {
-//         el.classList.remove('active');
-//         filter = '';
-//       } else {
-//         filters.forEach(tag => tag.classList.remove('active'));
-//         el.classList.add('active');
-//         filter = e.target.dataset.filter;
-//       }
-//       showTodos();
-//     });
-//   });
-
 
 displayTask();
